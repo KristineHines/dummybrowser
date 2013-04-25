bb147a9f491bfdbaab4f3e750647eb1c094b3432
Apprentice <apprentice@devbootcamp.com>
2013-04-24 17:54:29 -0700

    the whole thing

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,40 @@
+enable :sessions
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+get '/create' do
+  erb :create
+end
+
+post '/create' do
+  User.create(params)
+  @message = "Login with your new account details"
+  erb :index
+end
+
+post '/login' do
+  if User.login(params)
+    @user = User.find_by_email(params[:email])
+    session[:id] = @user.id
+    erb :secret_page
+  else
+    @message = "Error, either username or password incorrect"
+    erb :index
+  end
+end
+
+get '/logout' do
+  session.clear
+  erb :index
+end
+
+get '/secret' do
+  if session[:id] 
+    erb :secret_page
+  else
+    @message = "You must be logged in to see that page!"
+    erb :index
+  end
 end
